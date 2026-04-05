2cd15faa2673ed755fa3c0b0b6e5b52225e623cd
RAHULKUMAR JHA <jq8ntvvw5b@privaterelay.appleid.com>
2026-04-05 20:29:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -193,4 +193,5 @@ def on_frame(data):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    print("Server starting on http://localhost:5000")
+    socketio.run(app, host="0.0.0.0", port=5000)
